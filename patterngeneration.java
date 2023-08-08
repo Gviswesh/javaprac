aa7f0e60533a3887d35c56a4e69f2e7da9dcbb6e
Viswesh G <visweshsubramaian@outlook.com>
2023-08-08 22:50:30 +0530

    Minor changes for more accurate output

--- patterngeneration.java
+++ patterngeneration.java
@@ -30,18 +30,27 @@ import java.util.Scanner;
 public class patterngeneration {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter a number: ");
-        int n = scanner.nextInt();
-
-        generatePattern(n);
+        System.out.print("Enter a number N: ");
+        int N = scanner.nextInt();
+        
+        generatePattern(N);
     }
 
-    public static void generatePattern(int n) {
-        for (int i = 1; i <= 2 * n - 1; i++) {
-            for (int j = 1; j <= 2 * n - 1; j++) {
-                int num = Math.max(Math.abs(i - n + 1), Math.abs(j - n + 1)) + 1;
-                System.out.print(num);
-                if (j < 2 * n - 1) {
+    public static void generatePattern(int N) {
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i);
+                if (j < i) {
+                    System.out.print("*");
+                }
+            }
+            System.out.println();
+        }
+
+        for (int i = N - 1; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i);
+                if (j < i) {
                     System.out.print("*");
                 }
             }
